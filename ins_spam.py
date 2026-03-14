a6baaef9c14145a900fe13013775c1a250018768
Srinjoy Chatterjee <66124383+Srinjoy226440@users.noreply.github.com>
2026-03-15 00:15:47 +0530

    Add files via upload

--- ins_spam.py
+++ ins_spam.py
@@ -1,13 +1,13 @@
 import pyautogui
 import time
 
-print("Let's go")
+print("do you")
 
 time.sleep(5) # Give yourself time to position the mouse
 pyautogui.click() # Clicks the box to make it 'active'
 
 for i in range(20):
-    # Typing with an interval makes it look human to Instagram
+    # Typing with an interval makes it look human
     pyautogui.typewrite("Hello Tanvi", interval=0.1) 
     time.sleep(0.5)
     pyautogui.press('enter') 
